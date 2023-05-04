8a63c43659be50698e411fc908ed0d9cf798d5af
aybarsyazici <aybarsyazici@sabanciuniv.edu>
2023-05-04 19:46:49 +0200

    Updates

---
+++ static/js/4730.52532152.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 4730.52532152.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4730],{94730:function(e,n,t){t.r(n),t.d(n,{CalciteTooltip:function(){return p},defineCustomElement:function(){return c}});var u=t(36290),p=u.T,c=u.d}}]);
+//# sourceMappingURL=4730.52532152.chunk.js.map
