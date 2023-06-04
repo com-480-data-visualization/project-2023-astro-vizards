6a3a0520c7b35f4e804ae8fd7ae081ee06722d75
cankirimca <cankirmca@gmail.com>
2023-06-04 13:47:39 +0200

    modify aboutus

--- app/src/routes/AboutUs/AboutUs.tsx
+++ app/src/routes/AboutUs/AboutUs.tsx
@@ -1,10 +1,11 @@
 import "./about-us.scss";
 import React, { useEffect } from 'react';
-import { Typography, Divider, Avatar, Space, Row, Col } from "antd";
+import { Typography, Divider, Avatar, Space, Row, Col} from "antd";
 
 const { Title, Paragraph } = Typography;
 
 export const AboutUs = () => {
+
   return (
     <div className="about-us">
       <div className="about">
@@ -15,22 +16,28 @@ export const AboutUs = () => {
           As the number of satellites orbiting the Earth continues to increase, tracking their movements and positions 
           has become further important. Our satellite position visualization project aims to provide a dynamic and 
           interactive way for our users to visualize the current positions of these objects in space by leveraging 
-          real-time data from various sources. We allow users to see the altitude, speed and other 
-          relevant information for the satellite of their choice.
+          real-time data from various sources. We allow users to see the satellites orbiting the Earth on 2D and 3D maps, 
+          and also view the altitude, speed and other relevant information for the satellite of their choice.
         </Paragraph>
       </div>
+
       <div className="data">
         <Title level={2}>About the Data</Title>
         <Divider />
-        <Paragraph>
-        Our data contains information about satellites currently in orbit, that we join together from two sources. First, 
-        we utilize the available information from a <a href="https://www.kaggle.com/datasets/ucsusa/active-satellites"> Kaggle Dataset </a>   
-        that contains general information about them, including the reference number that uniquely identifies the satellite.
-        We also use information about the orbits of the satellites that we obtain from <a href="https://celestrak.org/NORAD/elements/table.php?GROUP=active&FORMAT=tle">NORAD</a>
-        {' '}in TLE (Two-line element set) format.
-               
-        </Paragraph>
+        <Paragraph>Our dataset contains information about the satellites currently in the Earth's orbit. We obtain this dataset by joining two sources of data:
+        <ul>
+          <li>  A dataset gathered from <a href="https://celestrak.org/NORAD/elements/table.php?GROUP=active&FORMAT=tle">NORAD</a> that contains a unique reference number and name for each satellite, and also its orbit information in TLE format. This information enables us to compute the coordinates of a satellite at any time instant.</li>
+          <li>  A <a href="https://www.kaggle.com/datasets/ucsusa/active-satellites"> Kaggle Dataset </a> containing additional information on the satellites such as:
+            <ul>
+              <li> The country and organization operating the satellite</li>
+              <li> Users of the satellite (Government, commercial etc.)</li>
+              <li> Purpose of use (Communications, observation etc.)</li>
+            </ul>
+          </li>
+        </ul>
+        </Paragraph>   
       </div>
+
       <div className="team">
         <Title level={2}>About the Team</Title>
         <Divider />
